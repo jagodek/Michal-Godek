445b56fd685a4296745f622255971c78a57c5d90
jagodek <godek.main@gmail.com>
2023-12-07 23:06:02 +0100

    check if multiple htmls work

---
+++ scripts/background.js
@@ -0,0 +1,5 @@
+let noRepeatButton = document.getElementById('no-repeat-button');
+noRepeatButton.addEventListener('click', function(){
+    let bodyClassList = document.getElementsByTagName('body')[0].setAttribute('class', 'no-repeat');
+
+});
