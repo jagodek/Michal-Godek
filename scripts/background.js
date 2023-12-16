9a6c9978d6929a9e28eb8ed3c8bb0d78f832d2f2
jagodek <godek.main@gmail.com>
2023-12-16 16:04:23 +0100

    redo

--- scripts/background.js
+++
@@ -1,5 +0,0 @@
-let noRepeatButton = document.getElementById('no-repeat-button');
-noRepeatButton.addEventListener('click', function(){
-    let bodyClassList = document.getElementsByTagName('body')[0].setAttribute('class', 'no-repeat');
-
-});
